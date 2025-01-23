0c9816301e53390503609c5d037128a2781e357b
liloudemierre <161479296+liloudemierre@users.noreply.github.com>
2025-01-23 09:48:45 +0100

    terminé

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,31 @@ function toggleColor() {
   box2.classList.toggle("is-active");
 }
 box2.addEventListener("click", toggleColor);
+
+var box3 = document.querySelector(".trigger");
+
+var box4 = document.querySelector(".target");
+
+function trigger() {
+  box4.classList.toggle("is-hidden");
+}
+
+box3.addEventListener("click", trigger);
+
+var box5 = document.querySelector(".trigger-all");
+
+function triggerAll() {
+  box1.classList.toggle("is-active");
+  box2.classList.toggle("is-active");
+}
+box5.addEventListener("click", triggerAll);
+
+var box6 = document.querySelector(".trigger-all-self");
+
+function triggerAllSelf() {
+  box1.classList.toggle("is-active");
+  box2.classList.toggle("is-active");
+  box6.classList.toggle("is-active");
+}
+
+box6.addEventListener("click", triggerAllSelf);
